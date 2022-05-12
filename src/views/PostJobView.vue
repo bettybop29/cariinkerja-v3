a2d27e49c1eadfc478de2a2e1bdcd3e00c8bebcb
bettybop29 <babyimoneter@gmail.com>
2022-05-12 12:34:25 +0700

    last upadate

--- src/views/PostJobView.vue
+++ src/views/PostJobView.vue
@@ -9,7 +9,7 @@
     </button>
 <form>
     <div class="modal fade" id="jobModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel1">Add Job</h5>
